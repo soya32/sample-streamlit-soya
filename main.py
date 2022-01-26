c10154f4955ca0cebe091cf56ddb4f3ecf63f86f
soya32 <souma32_yamamoto@keio.jp>
2022-01-26 16:46:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ if uploaded_file is not None:
         draw = ImageDraw.Draw(img)
         draw.rectangle([(rect['left'], rect['top']), (rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='green', width=5)
         atr = result['faceAttributes']
-    emo = atr['emotion']
-    neu = emo['neutral']
-    con = neu * 100
-    conc = str(con)
-    
-    textsize = 50
-    font = ImageFont.truetype("Arial Unicode.ttf", size=textsize)
-    txpos = (rect['left'],rect['top']-textsize-5)
-    draw.text(txpos, conc, font=font, fill='white')   
+        emo = atr['emotion']
+        neu = emo['neutral']
+        con = neu * 100
+        conc = str(con)
+        
+        textsize = 50
+        font = ImageFont.truetype("Arial Unicode.ttf", size=textsize)
+        txpos = (rect['left'],rect['top']-textsize-5)
+        draw.text(txpos, conc, font=font, fill='white')   
     st.image(img, caption="Uploaded image.", use_column_width=True)
